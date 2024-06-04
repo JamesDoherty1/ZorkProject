08b2300b3d1ca1fa95c699a09e3ba4b3bb5ba310
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:26:22 +0100

    Update GameException.h

--- GameException.h
+++ GameException.h
@@ -1,1 +1,18 @@
+#ifndef ZORKPROJECT_GAMEEXCEPTION_H
+#define ZORKPROJECT_GAMEEXCEPTION_H
+
+
+#include <exception>
+#include <string>
+
+class GameException : public std::exception {
+private:
+    std::string message;
+public:
+    GameException(const std::string& msg);
+
+    const char* what() const noexcept override;
+};
+
+#endif //ZORKPROJECT_GAMEEXCEPTION_H
 
