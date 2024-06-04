379e21add17866bae4993dfe35cf7186c16d5236
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:22:12 +0100

    Update Attribute.h

--- Attribute.h
+++ Attribute.h
@@ -1,1 +1,25 @@
+#ifndef ATTRIBUTE_H_
+#define ATTRIBUTE_H_
+
+template<typename T>
+class Attribute {
+private:
+    T value;
+
+public:
+    Attribute() : value(T()) {}
+
+    Attribute(T val) : value(val) {}
+
+    Attribute<T>& operator+=(const T& rhs) {
+        value += rhs;
+        return *this;
+    }
+
+    T getValue() const {
+        return value;
+    }
+};
+
+#endif // ATTRIBUTE_H_
 
