39879c570678c63dedbe3a30cbd9397cf7037e4b
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:27:30 +0100

    Update Room.cpp

--- Room.cpp
+++ Room.cpp
@@ -1,90 +1,103 @@
 #include "Room.h"
-#include "Command.h"
+#include "ZorkUL.h"
 
-
-Room::Room(string description) {
-	this->description = description;
+Room::Room(string description, Player& player, ZorkUL& zorkUL)
+    : description(description), player(player), zorkUL(zorkUL) {
 }
 
-void Room::setExits(Room *north, Room *east, Room *south, Room *west) {
-	if (north != NULL)
-		exits["north"] = north;
-	if (east != NULL)
-		exits["east"] = east;
-	if (south != NULL)
-		exits["south"] = south;
-	if (west != NULL)
-		exits["west"] = west;
+Room::Room(const Room& other)
+    : description(other.description), exits(other.exits), player(other.player), itemsInRoom(other.itemsInRoom), zorkUL(other.zorkUL) {
+}
+
+void Room::setExits(Room* north, Room* east, Room* south, Room* west) {
+    if (north != nullptr) exits["north"] = north;
+    if (east != nullptr) exits["east"] = east;
+    if (south != nullptr) exits["south"] = south;
+    if (west != nullptr) exits["west"] = west;
 }
 
 string Room::shortDescription() {
-	return description;
+    return description;
 }
 
-string Room::longDescription() {
-	return "room = " + description + ".\n" + displayItem() + exitString();
+string Room::equipmentDescription() {
+    stringstream descriptionStream;
+    zorkUL.GUIPrint("", "first");
+    descriptionStream << "room = " << description << ".\n" << exitString();
+    string description = descriptionStream.str();
+    description += "\n";
+    if (!itemsInRoom.empty()) {
+        description += "Items in room:\n";
+        for (const Item& item : itemsInRoom) {
+            if (!item.getIsAward()) {
+                description += "- " + item.getShortDescription() + "\n";
+            }
+        }
+    }
+    return description;
+}
+
+string Room::awardDescription(Room* currentRoom, int location) {
+    if (!itemsInRoom.empty() && currentRoom->shortDescription() != "entrance") {
+        zorkUL.GUIPrint("Well done, You have won the following items:", "first");
+        for (const Item& item : itemsInRoom) {
+            if (item.getIsAward()) {
+                player.addExperience(std::to_string(item.getAwardExperience().getValue()));
+                player.addStrength(std::to_string(item.getAwardStrength().getValue()));
+                zorkUL.GUIPrint("Total Experience: " + player.getExperience() + "  Total Strength: " + player.getStrength(), "score");
+                zorkUL.GUIPrint(item.getShortDescription(), "second");
+            }
+
+        }
+    }
+    else if(currentRoom->shortDescription() == "entrance"){
+        zorkUL.GUIPrint("Well done, You have opened the door", "first");
+        zorkUL.GUIPrint(currentRoom->exitString(), "second");
+    }
+    return "";
 }
 
 string Room::exitString() {
-	string returnString = "\nexits =";
-	for (map<string, Room*>::iterator i = exits.begin(); i != exits.end(); i++)
-		// Loop through map
-		returnString += "  " + i->first;	// access the "first" element of the pair (direction as a string)
-	return returnString;
+    string returnString = "Exits: ";
+    for (auto const& [direction, room] : exits) {
+        returnString += direction + " ";
+    }
+    return returnString;
 }
 
 Room* Room::nextRoom(string direction) {
-	map<string, Room*>::iterator next = exits.find(direction); //returns an iterator for the "pair"
-	if (next == exits.end())
-		return NULL; // if exits.end() was returned, there's no room in that direction.
-	return next->second; // If there is a room, remove the "second" (Room*)
-				// part of the "pair" (<string, Room*>) and return it.
+    if (exits.find(direction) != exits.end()) {
+        return exits[direction];
+    }
+    return nullptr;
 }
 
-void Room::addItem(Item *inItem) {
-    //cout <<endl;
-    //cout << "Just added" + inItem->getLongDescription();
+void Room::addItem(Item* inItem) {
     itemsInRoom.push_back(*inItem);
 }
 
-string Room::displayItem() {
-    string tempString = "items in room = ";
-    int sizeItems = (itemsInRoom.size());
-    if (itemsInRoom.size() < 1) {
-        tempString = "no items in room";
+int Room::isItemInRoom(string inString) {
+    for (size_t i = 0; i < itemsInRoom.size(); ++i) {
+        if (itemsInRoom[i].getShortDescription() == inString) {
+            return i;
         }
-    else if (itemsInRoom.size() > 0) {
-       int x = (0);
-        for (int n = sizeItems; n > 0; n--) {
-            tempString = tempString + itemsInRoom[x].getShortDescription() + "  " ;
-            x++;
-            }
-        }
-    return tempString;
     }
-
-int Room::numberOfItems() {
-    return itemsInRoom.size();
-}
-
-int Room::isItemInRoom(string inString)
-{
-    int sizeItems = (itemsInRoom.size());
-    if (itemsInRoom.size() < 1) {
-        return false;
-        }
-    else if (itemsInRoom.size() > 0) {
-       int x = (0);
-        for (int n = sizeItems; n > 0; n--) {
-            // compare inString with short description
-            int tempFlag = inString.compare( itemsInRoom[x].getShortDescription());
-            if (tempFlag == 0) {
-                itemsInRoom.erase(itemsInRoom.begin()+x);
-                return x;
-            }
-            x++;
-            }
-        }
     return -1;
 }
+
+bool Room::isEmpty() const {
+    return itemsInRoom.empty();
+}
+
+const vector<Item>& Room::getItems() const {
+    return itemsInRoom;
+}
+
+void Room::removeItem(int index) {
+    if (index >= 0 && index < itemsInRoom.size()) {
+        itemsInRoom.erase(itemsInRoom.begin() + index);
+    }
+}
+
+
 
