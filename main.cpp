9733d01f81256ece565d41f2ca6b45ca40574a17
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:30:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-//#include <iostream>
+#include <QApplication>
+#include "MainWindow.h"
 
-//using namespace std;
-
-//int main()
-//{
-//    cout << "Hello World!" << endl;
-//    return 0;
-//}
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
