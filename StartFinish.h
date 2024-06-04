428d8a457bcaf3e9430d3e1146743539b2cdfe50
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:28:18 +0100

    Update StartFinish.h

--- StartFinish.h
+++ StartFinish.h
@@ -1,1 +1,22 @@
+#ifndef ZORKPROJECT_STARTFINISH_H
+#define ZORKPROJECT_STARTFINISH_H
+
+#include <string>
+
+using namespace std;
+
+class StartFinish {
+public:
+    string displayMessage(string startFinish);
+
+
+private:
+    string startFinish;
+    string startFinishMessage;
+};
+
+
+
+
+#endif //ZORKPROJECT_STARTFINISH_H
 
