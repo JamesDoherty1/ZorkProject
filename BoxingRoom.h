5f4dd04406c51d947eeb49d6a28c01f1878675c3
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:22:45 +0100

    Update BoxingRoom.h

--- BoxingRoom.h
+++ BoxingRoom.h
@@ -1,1 +1,21 @@
+#ifndef BOXINGROOM_H_
+#define BOXINGROOM_H_
+
+#include "Room.h"
+#include "Player.h"
+#include "StartFinish.h"
+#include "ZorkUL.h"
+#include <QTimer>
+
+class BoxingRoom : public Room, public StartFinish {
+    Q_OBJECT
+
+public:
+    BoxingRoom(std::string description, Player& player, ZorkUL& zorkUL);
+
+    std::string awardDescription(Room* currentRoom, int location) override;
+    Room* nextRoom(std::string direction) override;
+};
+
+#endif // BOXINGROOM_H_
 
