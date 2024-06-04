a1b6f32e394a7882409540d1aec5a1712b5647ab
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:25:26 +0100

    Update EntranceRoom.cpp

--- EntranceRoom.cpp
+++ EntranceRoom.cpp
@@ -1,1 +1,13 @@
+#include "EntranceRoom.h"
+#include <iostream>
+
+EntranceRoom::EntranceRoom(std::string description, Player& player, ZorkUL& zorkUL)
+    : Room(description, player, zorkUL) {
+    unlocked = false;
+}
+
+void EntranceRoom::unlockRoom() {
+    unlocked = true;
+}
+
 
