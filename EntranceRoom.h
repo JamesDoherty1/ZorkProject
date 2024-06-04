3ab6226531396253f3b936e80d54bf9b773ed7f7
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:25:44 +0100

    Update EntranceRoom.h

--- EntranceRoom.h
+++ EntranceRoom.h
@@ -1,1 +1,19 @@
+#ifndef ZORKPROJECT_ENTRANCEROOM_H
+#define ZORKPROJECT_ENTRANCEROOM_H
+
+#include "Room.h"
+#include "Player.h"
+#include "StartFinish.h"
+#include "ZorkUL.h"
+
+class EntranceRoom : public Room, public StartFinish {
+
+public:
+    EntranceRoom(std::string description, Player& player, ZorkUL& zorkUL);
+    bool unlocked;
+    void unlockRoom();
+
+};
+
+#endif // ZORKPROJECT_ENTRANCEROOM_H
 
