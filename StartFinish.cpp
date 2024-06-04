97244d84a7568b38f0abf06f2282f2c6ef9a8bd4
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:28:04 +0100

    Update StartFinish.cpp

--- StartFinish.cpp
+++ StartFinish.cpp
@@ -1,1 +1,7 @@
+#include "StartFinish.h"
+
+string StartFinish::displayMessage(string startFinish) {
+    startFinishMessage = "You are " + startFinish + " the game"; // Assign value
+    return startFinishMessage;
+}
 
