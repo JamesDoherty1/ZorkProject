2b00313d43e5a9c31bc8191513c33da684ea2bed
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2024-06-04 20:28:49 +0100

    Update ZorkUL.h

--- ZorkUL.h
+++ ZorkUL.h
@@ -1,30 +1,53 @@
-#ifndef ZORKUL_H_
-#define ZORKUL_H_
+#ifndef ZORKUL_H
+#define ZORKUL_H
 
+#include <QObject>
+#include "Room.h"
+#include "Item.h"
 #include "Command.h"
 #include "Parser.h"
-#include "Room.h"
-#include "item.h"
-#include <iostream>
-#include <string>
-using namespace std;
+#include "Player.h"
 
-class ZorkUL {
-private:
-	Parser parser;
-	Room *currentRoom;
-	void createRooms();
-	void printWelcome();
-	bool processCommand(Command command);
-	void printHelp();
-	void goRoom(Command command);
-    void createItems();
-    void displayItems();
+class Room;
+
+class ZorkUL : public QObject {
+    Q_OBJECT
 
 public:
-	ZorkUL();
-	void play();
-	string go(string direction);
+    ZorkUL();
+    void createRooms();
+    void printWelcome();
+    bool processCommand(Command command);
+    void printHelp();
+    void goRoom(Command command);
+    void GUIPrint(string message, string type);
+    string go(string direction);
+    bool update(const string& userInput);
+    void takeItem (string itemName);
+    void performAction ();
+    string getCurrentItem();
+
+signals:
+    void requestUserInput(const QString& prompt);
+    void closeGameWindow();
+    void setImage(const QString& prompt);
+    void takeButton(const QString& prompt);
+    void actionButton(const QString& prompt);
+    void secondMessage(const QString& message);
+    void firstMessage(const QString& message);
+    void topMessage(const QString& message);
+    void scoreMessage(const QString& message);
+
+public slots:
+    void closeWindow();
+
+private:
+    Room* currentRoom;
+    Parser parser;
+    Player player;
+    Item takenItem;
+    std::string equipmentAction;
+    int location;
 };
 
-#endif /*ZORKUL_H_*/
+#endif // ZORKUL_H
